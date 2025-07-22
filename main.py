505b222be5505bf1e1d207a56854b0a6bcc062d6
MrEdzilla <43966574+MrEdzilla@users.noreply.github.com>
2025-07-22 19:29:37 +0100

    Operação '/' adicionada
    Operação só pode decorrer se o denominador for diferente de 0; como tal, adiciona-se um ifelse

--- main.py
+++ main.py
@@ -14,7 +14,11 @@ def calculadora(num1: float, num2: float, operador: str) -> float:
         result = num1 - num2
     elif operador == '*':
         result = num1 * num2
-
+    elif operador == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            raise ZeroDivisionError("Não é possível dividir por zero!")
 
     return result
 
